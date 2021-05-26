2468d15b5ca90e92157be9cbd3daf98de1f8b886
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2021-05-27 00:39:37 +0530

    added visual graphs

--- lib/widgets/statistics_graphs.dart
+++ lib/widgets/statistics_graphs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
 
 class StatisticsGraphs extends StatelessWidget {
   const StatisticsGraphs({
@@ -7,6 +8,47 @@ class StatisticsGraphs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var confirmedData = [
+      1.5,
+      1.0,
+      1.9,
+      2.0,
+      4.0,
+      4.5,
+      5.1,
+      6.0,
+    ];
+
+    var activeData = [
+      4.5,
+      5.1,
+      6.0,
+      2.0,
+      4.0,
+      4.5,
+      5.1,
+      6.0,
+    ];
+    var recoveredData = [
+      1.9,
+      1.8,
+      1.6,
+      1.7,
+      1.4,
+      4.5,
+      5.1,
+      6.0,
+    ];
+    var deceasedData = [
+      1.9,
+      1.8,
+      1.6,
+      1.7,
+      1.4,
+      1.5,
+      1.4,
+      1.5,
+    ];
     return Container(
       color: Colors.white,
       width: double.infinity,
@@ -24,6 +66,64 @@ class StatisticsGraphs extends StatelessWidget {
                       elevation: 3,
                       shadowColor: Colors.black,
                       child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'CONFIRMED',
+                                style: TextStyle(
+                                  color: Colors.grey.shade900,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '790',
+                                    style: TextStyle(
+                                      color: Colors.pink.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_upward_outlined,
+                                    color: Colors.pink.shade600,
+                                    size: 18,
+                                  ),
+                                  Text(
+                                    '19',
+                                    style: TextStyle(
+                                      color: Colors.pink.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Sparkline(
+                                data: confirmedData,
+                                fallbackHeight: 20,
+                                fallbackWidth: 20,
+                                lineColor: Colors.pink.shade600,
+                                pointColor: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.rectangle,
@@ -39,6 +139,64 @@ class StatisticsGraphs extends StatelessWidget {
                       elevation: 3,
                       shadowColor: Colors.black,
                       child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'ACTIVE',
+                                style: TextStyle(
+                                  color: Colors.grey.shade900,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '626',
+                                    style: TextStyle(
+                                      color: Colors.blue.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_downward_outlined,
+                                    color: Colors.blue.shade600,
+                                    size: 18,
+                                  ),
+                                  Text(
+                                    '9',
+                                    style: TextStyle(
+                                      color: Colors.blue.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Sparkline(
+                                data: activeData,
+                                fallbackHeight: 20,
+                                fallbackWidth: 20,
+                                lineColor: Colors.blue.shade600,
+                                pointColor: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.rectangle,
@@ -50,7 +208,7 @@ class StatisticsGraphs extends StatelessWidget {
                   ],
                 ),
                 SizedBox(
-                  height: 12.0,
+                  height: 15.0,
                 ),
                 Row(
                   children: [
@@ -58,6 +216,64 @@ class StatisticsGraphs extends StatelessWidget {
                       elevation: 3,
                       shadowColor: Colors.black,
                       child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'RECOVERED',
+                                style: TextStyle(
+                                  color: Colors.grey.shade900,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '67',
+                                    style: TextStyle(
+                                      color: Colors.green.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_upward_outlined,
+                                    color: Colors.green.shade600,
+                                    size: 18,
+                                  ),
+                                  Text(
+                                    '17',
+                                    style: TextStyle(
+                                      color: Colors.green.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Sparkline(
+                                data: recoveredData,
+                                fallbackHeight: 20,
+                                fallbackWidth: 20,
+                                lineColor: Colors.green.shade600,
+                                pointColor: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.rectangle,
@@ -73,6 +289,64 @@ class StatisticsGraphs extends StatelessWidget {
                       elevation: 3,
                       shadowColor: Colors.black,
                       child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'DECEASED',
+                                style: TextStyle(
+                                  color: Colors.grey.shade900,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '18',
+                                    style: TextStyle(
+                                      color: Colors.grey.shade700,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_upward_outlined,
+                                    color: Colors.grey.shade700,
+                                    size: 18,
+                                  ),
+                                  Text(
+                                    '2',
+                                    style: TextStyle(
+                                      color: Colors.grey.shade700,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Sparkline(
+                                data: deceasedData,
+                                fallbackHeight: 20,
+                                fallbackWidth: 20,
+                                lineColor: Colors.grey.shade700,
+                                pointColor: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.rectangle,
